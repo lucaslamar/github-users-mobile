a57401e111d4bba8456e7d964fcbe712dbab5ff2
Lucas Lamar <lucass.lamar@gmail.com>
2020-03-13 20:49:51 -0300

    fix configura paginação e refresh

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly',
   },
   parser: 'babel-eslint',
   parserOptions: {
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import api from '../../services/api';
 
 import {
@@ -26,59 +25,26 @@ export default class User extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
-      navigate: PropTypes.func,
     }).isRequired,
   };
 
   state = {
     stars: [],
-    page: 1,
     loading: true,
-    refreshing: false,
   };
 
   async componentDidMount() {
-    this.load();
-  }
-
-  load = async (page = 1) => {
-    const { stars } = this.state;
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`, {
-      params: { page },
-    });
+    const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({
-      stars: page >= 2 ? [...stars, ...response.data] : response.data,
-      page,
-      loading: false,
-      refreshing: false,
-    });
-  };
-
-  loadMore = () => {
-    const { page } = this.state;
-
-    const nextPage = page + 1;
-
-    this.load(nextPage);
-  };
-
-  refreshList = () => {
-    this.setState({ refreshing: true, stars: [] }, this.load);
-  };
-
-  handleNavigate = repository => {
-    const { navigation } = this.props;
-
-    navigation.navigate('Repository', { repository });
-  };
+    this.setState({ stars: response.data, loading: false });
+  }
 
   render() {
     const { navigation } = this.props;
-    const { stars, loading, refreshing } = this.state;
+    const { stars, loading } = this.state;
 
     const user = navigation.getParam('user');
 
@@ -89,19 +55,14 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-
         {loading ? (
           <Loading />
         ) : (
           <Stars
             data={stars}
-            onRefresh={this.refreshList}
-            refreshing={refreshing}
-            onEndReachedThreshold={0.2}
-            onEndReached={this.loadMore}
             keyExtractor={star => String(star.id)}
             renderItem={({ item }) => (
-              <Starred onPress={() => this.handleNavigate(item)}>
+              <Starred>
                 <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
                 <Info>
                   <Title>{item.name}</Title>
